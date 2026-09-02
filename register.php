52fd165b01fbb6a0efec847305a81ad7abe0e3de
Shakthivarshini <shakthivarshini1211@gmail.com>
2026-09-02 22:04:57 +0530

    2026-09-02: Created home,student registration page

---
+++ register.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Student Registration<h1>
+
+    <form method ="post">
+        <p>
+            <label>Student Name: </label><br>
+            <input type="text" name="name"
+            required>
+       </p>
+       <p>
+        <label>Email: </label><br>
+            <input type="email" name="email"
+            required>
+</p>
+<p>
+    <label>Password: </label><br>
+            <input type="password" name="password"
+            required>
+</p>
+<p>
+    <input type="submit" value="Register">
+</form>
+
+<a href="login.php">Already have an account? Login</a>
+<br><br>
+<a href="index.html">Back to Home</a>
+ 
+</body>
+</html>
